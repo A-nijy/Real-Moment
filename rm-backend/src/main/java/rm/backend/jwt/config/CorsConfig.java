f674dfd67105ab5f94466e188b90e2e38df964d6
A-nijy <rainick98@gmail.com>
2024-11-05 22:48:10 +0900

    CORS 설정 수정 (모든 요청의 Origin 허용 -> 특정 요청의 Origin 허용)

--- rm-backend/src/main/java/rm/backend/jwt/config/CorsConfig.java
+++ rm-backend/src/main/java/rm/backend/jwt/config/CorsConfig.java
@@ -20,34 +20,26 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowCredentials(true);                                                            // 내 서버가 응답할 때 JSON을 js에서 처리할 수 있게 할지를 설정
-        config.addAllowedOriginPattern("*");                                                                // 모든 ip에 응답을 허용
-        config.addAllowedHeader("*");                                                                // 모든 header에 응답을 허용
-        config.addAllowedMethod("*");                                                                // 모든 http Method (post, get 등등)의 요청을 허용
+        config.setAllowCredentials(true);
+        // 인증 정보를 포함한 요청을 허용
+        config.addAllowedOriginPattern("https://real-moment.kro.kr");
+        config.addAllowedOriginPattern("https://real-moment-admin.kro.kr");
+        config.addAllowedOriginPattern("http://43.202.60.88");
+        // 모든 요청의 Origin을 허용
+        config.addAllowedHeader("*");
+        // 모든 요청의 헤더를 허용
+        config.addAllowedMethod("*");
+        // 모든 요청의 HTTP 메서드 허용
 
-        config.addExposedHeader(jwtProperties.getACCESS_STRING());                                                    // 리액트(브라우저)에서 헤더에 담긴 토큰접근 허용
+        config.addExposedHeader(jwtProperties.getACCESS_STRING());
         config.addExposedHeader(jwtProperties.getREFRESH_STRING());
+        // 클라이언트(리액트, 브라우저)가 응답 헤더에 담긴 토큰을 읽을 수 있도록 허용
 
-        source.registerCorsConfiguration("/**", config);                                     // /api/**로 들어오는 url에 대해서는 config대로 정의함
+        source.registerCorsConfiguration("/**", config);                                     
+        // /api/**로 들어오는 url(경로)에 해당 CORS규칙(config)을 적용
 
         System.out.println("Cors Filter 수행 완료");
 
         return new CorsFilter(source);
     }
 }
-
-
-//      아래꺼로 리액트와 연동 잘됨 내가 원래 작성한 위에 코드는 연결 안됨
-//
-//    @Bean
-//    public CorsFilter corsFilter() {
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(true);
-//        config.addAllowedOriginPattern("*");
-//        config.addAllowedHeader("*");
-//        config.addAllowedMethod("*");
-//
-//        source.registerCorsConfiguration("/**", config);
-//        return new CorsFilter(source);
-//    }
